a36665ed5d8f89ed24f9028cf45697ea773c7732
Beisenherz, Steffen [CBC] <steffen.beisenherz@rtl.de>
2025-05-22 15:48:41 +0200

    feat: add single dotfile management
    still todo:
    - [ ] git repository management
    - [ ] overwriting of existing files
    - [ ] better error handling

--- mac/src/lib.rs
+++ mac/src/lib.rs
@@ -1,1 +1,21 @@
+use anyhow::Result;
+use core::SetupAdapter;
+use std::env::consts;
 
+pub struct MacSetup {}
+
+impl SetupAdapter for MacSetup {
+    fn exec(
+        &self,
+        _config_string: &str,
+        _config_extension: &str,
+        _base_config: &core::basic_config::BasicConfigContent,
+    ) -> Result<()> {
+        match consts::OS {
+            "ios" | "macos" | "apple" => {}
+            _ => return Ok(()),
+        }
+
+        Ok(())
+    }
+}
